0e2f7e6d78f10423d4bd882b5fcd8c82248963e6
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2025-10-23 17:29:01 +0300

    ف

--- app/app.vue
+++ app/app.vue
@@ -22,9 +22,21 @@ useHead(useLocaleHead({
 <style  >
 body {
 font-family: Tajawal, sans-serif;
+
+  overflow-x: hidden; 
+  /* يضمن أن العرض لا يتجاوز 100% من النافذة */
+  width: 100%;
+
 }
 
+/* assets/css/main.css (أو ملف التنسيق الرئيسي) */
 
+body, html {
+  /* يمنع التمرير الأفقي ويخفي أي محتوى يخرج عن حدود العرض */
+  overflow-x: hidden; 
+  /* يضمن أن العرض لا يتجاوز 100% من النافذة */
+  width: 100%;
+}
 
 </style>
 
--- app/components/SmallNavbar.vue
+++ app/components/SmallNavbar.vue
@@ -50,7 +50,7 @@ const closeMenu = () => {
       'translate-y-0': isVisible 
     }">
     
-    <div class="bg-white/70 backdrop-blur-md shadow-xl flex justify-between items-center px-4 md:px-6 py-3">
+    <div class="bg-white shadow-xl flex justify-between items-center px-4 md:px-6 py-3">
       
       <img class="w-24 h-16 rounded-xl" src="~/assets/logo.png" alt="">
       
@@ -63,14 +63,14 @@ const closeMenu = () => {
     <Transition name="slide-down">
       <div v-if="isMenuOpen" 
            class="fixed top-0 left-0 w-full h-screen bg-white/70 backdrop-blur-md z-40 pt-[75px] shadow-2xl overflow-y-auto">
-           <div class="flex flex-col items-start px-8 py-4 space-y-6 text-xl">
+           <div class="flex flex-col items-start px-8 py-4 space-y-6 text-xl text-tow">
           
           <h1 class="w-full py-2 font-bold cursor-pointer hover:text-green-500 transition duration-300" @click="closeMenu">{{ $t('Home') }}</h1>
           <h1 class="w-full py-2 font-bold cursor-pointer hover:text-green-500 transition duration-300" @click="closeMenu">{{ $t('Our work') }}</h1>
           <h1 class="w-full py-2 font-bold cursor-pointer hover:text-green-500 transition duration-300" @click="closeMenu">{{ $t('Projects') }}</h1>
           <h1 class="w-full py-2 font-bold cursor-pointer hover:text-green-500 transition duration-300" @click="closeMenu">{{ $t('Blog') }}</h1>
 
-          <div class="w-full h-px bg-gray-200 my-2"></div> <h1 class="w-full pt-2 font-bold cursor-pointer text-2xl hover:text-green-500 transition duration-300 underline hover:no-underline" @click="closeMenu">{{ $t('Contact us') }}</h1>
+          <div class="w-full h-px bg-gray-200 my-2"></div> <h1 class="w-full  pt-2 font-bold cursor-pointer text-2xl hover:text-green-500 transition duration-300 underline hover:no-underline" @click="closeMenu">{{ $t('Contact us') }}</h1>
           
           <div class="w-full pt-4">
             <LanguageSwitcher />
