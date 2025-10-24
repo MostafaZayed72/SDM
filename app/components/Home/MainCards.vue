41bdedd523ff29376c9b8eed01db8522170568b4
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2025-10-24 22:51:36 +0300

    a

--- app/components/Home/MainCards.vue
+++ app/components/Home/MainCards.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-48">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4 md:px-48">
     <NuxtLink 
       v-for="(card, index) in projects"
       :key="index"
