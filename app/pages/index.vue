799f43bafd794dfd003aa5b69ecfb23ef2fd570d
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2025-10-25 13:53:10 +0300

    e

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,6 +5,6 @@
 <HomeMainCards class="pb-16"/>
 <HomeSubCards class="pb-16"/>
 <HomeSlider class="pb-16"/>
-<HomeBigComponent class="pb-16 px-8 mx-4 rounded-xl"/>
+<HomeBigComponent class="pb-16 px-8 mx-1 md:mx-4 rounded-xl"/>
 </div>
 </template>
